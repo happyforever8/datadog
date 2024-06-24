9943ece55c8dddd2a0646919c675fb15614986c7
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2024-06-23 20:20:18 -0400

    Update A_system_design.java

--- A_system_design.java
+++ A_system_design.java
@@ -1,2 +1,7 @@
 (1)System design: A system to detect if someone shares your photo on Instagram. 
     Assuming you have a Instagram post firehose, and ML model f‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌or facial detection.
+
+(2)第二轮，华人大哥，人不太nice，系统设计flight折扣原题，准备了很久，整个过程基本一气呵成。
+    大哥表示满意，但后面其中一个follow-up，关于为什么一个地方用DB存而不直接用queue。
+    可能没答出他想听到的，最后HR feedback显示挂在了这一轮。
+
