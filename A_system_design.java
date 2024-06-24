cff11f76ff1101030fd6c02d5ffe50cc04655e3b
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2024-06-23 20:19:10 -0400

    Create A_system_design.java

---
+++ A_system_design.java
@@ -0,0 +1,2 @@
+(1)System design: A system to detect if someone shares your photo on Instagram. 
+    Assuming you have a Instagram post firehose, and ML model f‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌or facial detection.
