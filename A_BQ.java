9d885fe0e0a3efc1c7bf43d6bac3e94c1957a4ae
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2024-06-23 20:31:41 -0400

    Update A_BQ.java

--- A_BQ.java
+++ A_BQ.java
@@ -8,3 +8,7 @@ HM behavior：
   我讲了不同的project，presentation那轮更重要些，所以把好的project在那一轮说
 
   experience轮详细问我之前的一个项目，让我画了这个项目的架构。
+
+  第四轮manager问past experience，就准备好讲一个past project，challenge在哪儿，
+  问得挺细，最好要讲自己真正做过的project。然后还问了几个BQ，就是很Frequent那种，遇到conflict怎么做啊什么的。
+
