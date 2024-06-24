a9bbfa4d6105c9f49154e16ec6d9e9b776f4235b
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2024-06-23 20:25:51 -0400

    Create A_BQ.java

---
+++ A_BQ.java
@@ -0,0 +1,8 @@
+past project presentation：
+这个感觉做的也还可以，主要是一个hiring team的director问了很多问题，最后还说这是一个很challenge很有意思的项目，然后就让我问问题了
+HM behavior：
+问了一堆标准问题。past project，how to handle conflict之类的
+最后挂了，感觉也是尽力了。 SD我觉得auth不是很重要的部分，就没太细说，
+  后来面试官就一直追着问auth怎么做，我心想auth不都是用cookie吗，都是标准做法，还有啥深入探讨的，‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌没想到面试的人就因为这个觉得我lead的不够，我也是醉了
+
+  我讲了不同的project，presentation那轮更重要些，所以把好的project在那一轮说
