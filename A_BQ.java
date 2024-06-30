7f715b5977af5820144ee0c34cfaf3c414c2cbc0
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2024-06-30 17:08:20 -0400

    Update A_BQ.java

--- A_BQ.java
+++ A_BQ.java
@@ -1,14 +1,19 @@
-past project presentation：
-这个感觉做的也还可以，主要是一个hiring team的director问了很多问题，最后还说这是一个很challenge很有意思的项目，然后就让我问问题了
-HM behavior：
-问了一堆标准问题。past project，how to handle conflict之类的
-最后挂了，感觉也是尽力了。 SD我觉得auth不是很重要的部分，就没太细说，
-  后来面试官就一直追着问auth怎么做，我心想auth不都是用cookie吗，都是标准做法，还有啥深入探讨的，‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌没想到面试的人就因为这个觉得我lead的不够，我也是醉了
+(1)past project presentation：
+    这个感觉做的也还可以，主要是一个hiring team的director问了很多问题，最后还说这是一个很challenge很有意思的项目，然后就让我问问题了
+    HM behavior：
+    问了一堆标准问题。past project，how to handle conflict之类的
+    最后挂了，感觉也是尽力了。 SD我觉得auth不是很重要的部分，就没太细说，
+      后来面试官就一直追着问auth怎么做，我心想auth不都是用cookie吗，都是标准做法，还有啥深入探讨的，‍‍‍‍‍‍‍‍‌‍‌‌‍‌‌‌‌‌没想到面试的人就因为这个觉得我lead的不够，我也是醉了
 
-  我讲了不同的project，presentation那轮更重要些，所以把好的project在那一轮说
 
-  experience轮详细问我之前的一个项目，让我画了这个项目的架构。
+    我讲了不同的project，presentation那轮更重要些，所以把好的project在那一轮说
+  
+    experience轮详细问我之前的一个项目，让我画了这个项目的架构。
+  
+    第四轮manager问past experience，就准备好讲一个past project，challenge在哪儿，
+    问得挺细，最好要讲自己真正做过的project。然后还问了几个BQ，就是很Frequent那种，遇到conflict怎么做啊什么的。
 
-  第四轮manager问past experience，就准备好讲一个past project，challenge在哪儿，
-  问得挺细，最好要讲自己真正做过的project。然后还问了几个BQ，就是很Frequent那种，遇到conflict怎么做啊什么的。
+  (2)第二轮bq：
+一个director面得，第一个proj大概聊了半个小时，具体除了很多细节技术和我的职责，
+  就是不经意间的bq常见问题： 组内组外协调，队员犯错或不同意见，遇到的困难，self motivation。后面时间不太够了有大概花了10min聊了聊其他的项目和我喜欢和讨厌现公司的点。
 
